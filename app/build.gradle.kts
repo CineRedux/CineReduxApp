a921de3d1a73a3ad4263d6df1809b875648594d8
MotjokaFanana <fmotjoka@gmail.com>
2024-10-14 17:18:35 +0200

    feat:added biometrics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation ("com.google.firebase:firebase-auth")
     implementation ("com.google.android.gms:play-services-auth:20.7.0")
     implementation ("androidx.preference:preference:1.2.0")
+    implementation ("androidx.biometric:biometric:1.2.0-alpha05")
 
 
 
